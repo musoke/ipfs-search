6af1789bd9cad310514d182b9593a591e770c5d6
Nathan Musoke <nathan.musoke@gmail.com>
2018-03-18 13:31:11 +1300

    Do very simple crawling from DHT requests
    Based on https://gkbrk.com/2018/03/writing-a-simple-ipfs-crawler/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate failure;
+extern crate ipfsapi;
+extern crate serde_json;
+
+use ipfsapi::IpfsApi;
+use failure::Error;
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap();
+}
+
+fn run() -> Result<(), Error> {
+    let api = IpfsApi::new("127.0.0.1", 5001);
+
+    let filtered_logs = api.log_tail()?
+        .filter(|l| l["event"].as_str() == Some("handleAddProvider"))
+        .filter(|x| x["key"].is_string());
+
+    for line in filtered_logs {
+        let hash = line["key"].as_str().unwrap();
+        println!("{}", hash);
+    }
+
+    Ok(())
 }

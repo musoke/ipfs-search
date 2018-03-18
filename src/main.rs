43679490a209b574d473f3091b1ed11ae0d37d25
Nathan Musoke <nathan.musoke@gmail.com>
2018-03-18 15:55:19 +1300

    Add simple search to test the index

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,13 @@ use std::path::Path;
 use tempdir::TempDir;
 use tantivy::schema::*;
 use tantivy::Index;
+use tantivy::collector::TopCollector;
+use tantivy::query::QueryParser;
 
 fn main() {
     if let Ok(dir) = TempDir::new("ipfs-search-index") {
         run_indexer(dir.path()).unwrap();
+        check_index(dir.path()).unwrap();
         dir.close().unwrap();
     }
 }
@@ -44,15 +47,42 @@ fn run_indexer(index_dir: &Path) -> Result<(), Error> {
         // TODO: fetch the hash and parse its content
 
         let schema_hash = schema.get_field("hash").expect("field name set during dev");
-        // let schema_body = schema.get_field("body").expect("field name set during dev");
+        let schema_body = schema.get_field("body").expect("field name set during dev");
         let mut doc = Document::default();
 
         doc.add_text(schema_hash, hash);
+        doc.add_text(schema_body, "some body?");
 
         index_writer.add_document(doc);
     }
 
     index_writer.commit().unwrap();
+
+    Ok(())
+}
+
+fn check_index(index_dir: &Path) -> Result<(), Error> {
+    println!("Checking index");
+
+    let index = Index::open(index_dir)?;
+    index.load_searchers()?;
+    let schema = index.schema();
+
+    let schema_hash = schema.get_field("hash").expect("field name set during dev");
+    let schema_body = schema.get_field("body").expect("field name set during dev");
+
+    let searcher = index.searcher();
+    let query_parser = QueryParser::for_index(&index, vec![schema_hash, schema_body]);
+
+    let query = query_parser.parse_query("some").unwrap();
+    let mut top_collector = TopCollector::with_limit(10);
+    searcher.search(&*query, &mut top_collector)?;
+    let doc_addresses = top_collector.docs();
+
+    for doc_address in doc_addresses {
+        let retrieved_doc = searcher.doc(&doc_address)?;
+        println!("{}", schema.to_json(&retrieved_doc));
+    }
 
     Ok(())
 }

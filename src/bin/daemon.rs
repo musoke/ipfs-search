f59caa4c2dc8ee915dec92e2e62d5c6111126406
Nathan Musoke <nathan.musoke@gmail.com>
2018-03-22 15:06:25 +1300

    Add basic logging

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -1,3 +1,6 @@
+extern crate env_logger;
+extern crate log;
+
 #[macro_use]
 extern crate clap;
 extern crate ipfs_search;
@@ -7,9 +10,25 @@ use tempdir::TempDir;
 use ipfs_search::run_indexer;
 use clap::{App, Arg, ArgMatches};
 use std::path::Path;
+use log::LevelFilter;
+use env_logger::Builder;
 
 fn main() {
     let matches = clap_app();
+
+    // Initialize logging
+    {
+        let mut log_builder = Builder::new();
+        let log_module = Some("ipfs_search");
+        match matches.occurrences_of("v") {
+            0 => log_builder.filter(log_module, LevelFilter::Error),
+            1 => log_builder.filter(log_module, LevelFilter::Warn),
+            2 => log_builder.filter(log_module, LevelFilter::Info),
+            3 => log_builder.filter(log_module, LevelFilter::Debug),
+            _ => log_builder.filter(None, LevelFilter::Trace),
+        };
+        log_builder.init();
+    }
 
     if let Some(dir) = matches.value_of("dir") {
         let path = Path::new(dir);
@@ -32,7 +51,13 @@ fn clap_app() -> ArgMatches<'static> {
                 .short("d")
                 .takes_value(true)
                 .value_name("DIR")
-                .help("directory containing index"),
+                .help("Directory containing index"),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
         );
 
     app.get_matches()
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+
 extern crate failure;
 extern crate ipfsapi;
 extern crate mime;
